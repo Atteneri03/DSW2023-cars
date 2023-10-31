b6b4ab8763ff0b0666c4f0da984e7077a7541053
Atteneri Estévez <atteeste2003@gmail.com>
2023-10-31 20:12:35 +0000

    add cars.json and modify controller

--- src/controllers/CarController.php
+++ src/controllers/CarController.php
@@ -5,9 +5,16 @@ class CarController {
   private $cars = [];
 
   public function __construct(){
-    $this->cars[] = new Car("1234","Ford","Kuga",2021,"blue");
-    $this->cars[] = new Car("3546","Seat","Cordoba",2002,"white");
-    $this->cars[] = new Car("2634","Seat","Ibiza",2019,"red");
+    // $this->cars[] = new Car("1234","Ford","Kuga",2021,"blue");
+    // $this->cars[] = new Car("3546","Seat","Cordoba",2002,"white");
+    // $this->cars[] = new Car("2634","Seat","Ibiza",2019,"red");
+    $json = file_get_contents("../data/cars.json");
+    $carsJSON = json_decode($json);
+    //$this->cars = $carsJSON;
+    foreach($carsJSON as $carJSON){
+      $this->cars[] = new Car($carJSON->id,$carJSON->make,$carJSON->model,$carJSON->year,$carJSON->color);
+
+    }
   }
 
   public function list() {
